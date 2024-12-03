0838df62d7bc7862d3e7f0cfdbf9d8208a5b4ad0
IanPages <ianpagesrodriguez@gmail.com>
2024-12-03 11:52:36 +0000

    continuacion

--- DEW/ut04-01/scripts/scripts.js
+++ DEW/ut04-01/scripts/scripts.js
@@ -1,5 +1,30 @@
+const DOM = {
+    form: document.getElementById("form"),
+    inputName: document.getElementById("NombreUsuario"),
+    contrasena: document.getElementById("Contrasena"),
+    fechaNacimiento: document.getElementById("nacimiento"),
+    titulo: document.getElementById("tit"),
+    descripcion: document.getElementById("desc"),
+}
+
+DOM.fechaNacimiento.addEventListener("click", () => {
+    for(let i = 1920; i <= 2010; i++ ){
+        let option = document.createElement("option");
+        option.append(i);
+        option.value = i
+        DOM.fechaNacimiento.append(option);
+    }
+})
+
+// (function() {
+
+
+// })();
+
+
+
 function mostrarPassword() {
-    var z = document.getElementById("passwd");
+    var z = document.getElementById("Contrasena");
     if (z.type === "password") {
         z.type ="text";
 
@@ -7,3 +32,20 @@ function mostrarPassword() {
         z.type= "password";
     }
 }
+
+
+DOM.titulo.addEventListener("change", () => {
+    var p = document.getElementById("longitudTitulo");
+    p.innerText = `${DOM.titulo.length} / 15`;
+})
+
+
+DOM.form.addEventListener("submit", (e) => {
+    if (!DOM.inputName.validationMessage==""){
+        e.preventDefault();
+        alert("El campo de username esta vacío");
+    }
+})
+
+
+
