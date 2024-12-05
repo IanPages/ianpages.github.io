5da8fe8ee9895cf02c0ad44880fc20e49fff323b
IanPages <ianpagesrodriguez@gmail.com>
2024-12-05 13:06:10 +0000

    Finalizar

--- DEW/ut04-01/scripts/scripts.js
+++ DEW/ut04-01/scripts/scripts.js
@@ -1,30 +1,35 @@
 const DOM = {
+    //descripciones generales
     form: document.getElementById("form"),
+    divValidation: document.getElementById("validationMessages"),
+    //inputs
     inputName: document.getElementById("NombreUsuario"),
     contrasena: document.getElementById("Contrasena"),
+    nombre: document.getElementById("Nombre"),
+    apellidos: document.getElementById("Apellidos"),
     fechaNacimiento: document.getElementById("nacimiento"),
+    telefono: document.getElementById("telefono"),
+    codPostal: document.getElementById("codpost"),
+    dniNie: document.getElementById("dni2"),
+    cuentaComo: document.getElementsByName("CuentaComo"),
     titulo: document.getElementById("tit"),
     descripcion: document.getElementById("desc"),
     longitudTitulo: document.getElementById("longitudTitulo"),
     longitudTextarea: document.getElementById("longitudTextarea"),
-    aficionesDiv: document.getElementById("aficionesReunidas")
-
+    aficionesDiv: document.getElementById("aficionesReunidas"),
+    //gestionErrores
+    errorUserName: document.getElementById("errorUserName"),
+    errorPassword: document.getElementById("errorPassword"),
+    errorNombre: document.getElementById("errorNombre"),
+    errorApellido: document.getElementById("errorApellido"),
+    errorTelefono: document.getElementById("errorTelefono"),
+    errorCodPostal: document.getElementById("errorCodPostal"),
+    errorDniNie: document.getElementById("errorDniNie"),
+    errorCuentaComo: document.getElementById("errorCuentaComo"),
+    errorAficiones: document.getElementById("errorAficiones"),
+    errorPublicacionTitulo: document.getElementById("errorPublicacionTitulo"),
+    errorPublicacionDescripcion: document.getElementById("errorPublicacionDescripcion"),
 }
-
-DOM.fechaNacimiento.addEventListener("click", () => {
-    for(let i = 1920; i <= 2010; i++ ){
-        let option = document.createElement("option");
-        option.append(i);
-        option.value = i
-        DOM.fechaNacimiento.append(option);
-    }
-})
-// (function() {
-
-
-// })();
-
-
 
 function mostrarPassword() {
     var z = document.getElementById("Contrasena");
@@ -36,6 +41,14 @@ function mostrarPassword() {
     }
 }
 
+DOM.fechaNacimiento.addEventListener("click", () => {
+    for(let i = 1921; i <= 2010; i++ ){
+        let option = document.createElement("option");
+        option.append(i);
+        option.value = i
+        DOM.fechaNacimiento.append(option);
+    }
+})
 
 DOM.titulo.addEventListener("input", () => {
     DOM.longitudTitulo.textContent = `${DOM.titulo.value.length} / 15`;
@@ -55,8 +68,8 @@ DOM.form.addEventListener("submit", (e) => {
     });
 
     if (aficionesElegidas.length <2){
+        DOM.errorAficiones.textContent="Minimo 2 selecciones"
         e.preventDefault();
-        return;
     }
 
     let inputHidden = document.createElement("input");
@@ -65,10 +78,52 @@ DOM.form.addEventListener("submit", (e) => {
     inputHidden.value= aficionesElegidas.join(',');
 
     DOM.aficionesDiv.append(inputHidden);
+    let campo = "Campo Obligatorio";
 
-    if (!DOM.inputName.validationMessage==""){
-        e.preventDefault();
+    let inputsRequired = document.querySelectorAll("input[required]");
+    
+    //FALTA AÑADIR QUE BORRE TODOS LOS HIJOS MEDIANTE DOM 
+    while (DOM.divValidation.firstChild) {
+        DOM.divValidation.removeChild(DOM.divValidation.firstChild);
     }
+    
+    inputsRequired.forEach(element => {
+        if (element.validationMessage!= ""){
+            let span = document.createElement("span");
+            span.textContent=`${element.name} = ${element.validationMessage}`
+            DOM.divValidation.append(span);
+            e.preventDefault();
+        }
+    });
+
+    if (DOM.inputName.value == ""){
+        DOM.errorUserName.textContent = campo;
+    }
+    if(DOM.contrasena.value == ""){
+        DOM.errorPassword.textContent= campo
+    }
+    if (DOM.nombre.value==""){
+        DOM.errorNombre.textContent=campo
+    }
+    if (DOM.apellidos.value == ""){
+        DOM.errorApellido.textContent =campo
+    }
+    if(DOM.telefono.value == ""){
+        DOM.errorTelefono.textContent= campo
+    }
+    if (DOM.codPostal.value==""){
+        DOM.errorCodPostal.textContent=campo
+    }
+    if (DOM.dniNie.value == ""){
+        DOM.errorDniNie.textContent=campo
+    }
+    if(DOM.titulo.value ==""){
+        DOM.errorPublicacionTitulo.textContent=campo
+    }
+    if(DOM.descripcion.value== ""){
+        DOM.errorPublicacionDescripcion.textContent=campo
+    }
+
 })
 
 
